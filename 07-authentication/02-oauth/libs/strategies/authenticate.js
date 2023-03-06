f4da0bd2dd8f8e052cf1300cf9ca1256fc5ad620
muryg <muryg@localhost>
2023-03-07 00:48:56 +0300

    add solution for 7-module 1-task

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,26 @@
+const User = require('../../models/User');
+
 module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+    
+    if (!email) return done(null, false, 'Не указан email');
+    
+    User.findOne({email: email}, function (err, user) {
+
+        if (err) return done(err);
+
+        if (!user) 
+        {
+            user = new User({
+                email: email,
+                displayName: displayName
+            });
+
+            user.save().then((user) => {
+                return done(null, user);
+            }, (err) => {
+                return done(err);                
+            });            
+        }
+        else return done(null, user);
+    });
 };
